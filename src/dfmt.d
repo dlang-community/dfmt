e4ed938515978b9d93001b7564b1fe7cdc29ac50
Hackerpilot <briancschott@gmail.com>
2015-01-25 10:57:31 -0800

    Fix #11

--- src/dfmt.d
+++ src/dfmt.d
@@ -43,7 +43,7 @@ Formats D code.
 ";
 
 version (NoMain)
-{ } 
+{ }
 else
 int main(string[] args)
 {
@@ -343,7 +343,7 @@ private:
             case tok!";":
                 tempIndent = 0;
                 writeToken();
-                if (current.type != tok!"comment")
+                if (index >= tokens.length || current.type != tok!"comment")
                     newline();
                 if (peekImplementation(tok!"class",0))
                     newline();
@@ -432,8 +432,9 @@ private:
         else if (current.type == tok!"identifier")
         {
             writeToken();
-            if (current.type == tok!"identifier" || isKeyword(current.type)
-                || current.type == tok!"@")
+            if (index < tokens.length && (current.type == tok!"identifier"
+				|| isKeyword(current.type) || isBasicType(current.type)
+				|| current.type == tok!"@"))
                 write(" ");
         }
         else
@@ -894,33 +895,32 @@ private:
 
 string generateFixedLengthCases()
 {
-    import std.algorithm:map;
-    import std.string:format;
+    import std.algorithm : map;
+    import std.string : format;
 
-    string[] fixedLengthTokens = [
-    "abstract", "alias", "align", "asm", "assert", "auto", "body", "bool",
-    "break", "byte", "case", "cast", "catch", "cdouble", "cent", "cfloat",
-    "char", "class", "const", "continue", "creal", "dchar", "debug", "default",
-    "delegate", "delete", "deprecated", "do", "double", "else", "enum",
-    "export", "extern", "false", "final", "finally", "float", "for", "foreach",
-    "foreach_reverse", "function", "goto", "idouble", "if", "ifloat",
-    "immutable", "import", "in", "inout", "int", "interface", "invariant",
-    "ireal", "is", "lazy", "long", "macro", "mixin", "module", "new", "nothrow",
-    "null", "out", "override", "package", "pragma", "private", "protected",
-    "public", "pure", "real", "ref", "return", "scope", "shared", "short",
-    "static", "struct", "super", "switch", "synchronized", "template", "this",
-    "throw", "true", "try", "typedef", "typeid", "typeof", "ubyte", "ucent",
-    "uint", "ulong", "union", "unittest", "ushort", "version", "void",
-    "volatile", "wchar", "while", "with", "__DATE__", "__EOF__", "__FILE__",
-    "__FUNCTION__", "__gshared", "__LINE__", "__MODULE__", "__parameters",
-    "__PRETTY_FUNCTION__", "__TIME__", "__TIMESTAMP__", "__traits", "__vector",
-    "__VENDOR__", "__VERSION__", ",", ".", "..", "...", "/", "/=", "!", "!<",
-    "!<=", "!<>", "!<>=", "!=", "!>", "!>=", "$", "%", "%=", "&", "&&", "&=",
-    "(", ")", "*", "*=", "+", "++", "+=", "-", "--", "-=", ":", ";", "<", "<<",
-    "<<=", "<=", "<>", "<>=", "=", "==", "=>", ">", ">=", ">>", ">>=", ">>>",
-    ">>>=", "?", "@", "[", "]", "^", "^=", "^^", "^^=", "{", "|", "|=", "||",
-    "}", "~", "~="
-    ];
-
-    return fixedLengthTokens.map!(a => format(`case tok!"%s": return %d;`, a, a.length)).join("\n\t");
+    string[] fixedLengthTokens = ["abstract", "alias", "align", "asm", "assert",
+        "auto", "body", "bool", "break", "byte", "case", "cast", "catch",
+        "cdouble", "cent", "cfloat", "char", "class", "const", "continue",
+        "creal", "dchar", "debug", "default", "delegate", "delete", "deprecated",
+        "do", "double", "else", "enum", "export", "extern", "false", "final",
+        "finally", "float", "for", "foreach", "foreach_reverse", "function",
+        "goto", "idouble", "if", "ifloat", "immutable", "import", "in", "inout",
+        "int", "interface", "invariant", "ireal", "is", "lazy", "long", "macro",
+        "mixin", "module", "new", "nothrow", "null", "out", "override", "package",
+        "pragma", "private", "protected", "public", "pure", "real", "ref",
+        "return", "scope", "shared", "short", "static", "struct", "super",
+        "switch", "synchronized", "template", "this", "throw", "true", "try",
+        "typedef", "typeid", "typeof", "ubyte", "ucent", "uint", "ulong", "union",
+        "unittest", "ushort", "version", "void", "volatile", "wchar", "while",
+        "with", "__DATE__", "__EOF__", "__FILE__", "__FUNCTION__", "__gshared",
+        "__LINE__", "__MODULE__", "__parameters", "__PRETTY_FUNCTION__",
+        "__TIME__", "__TIMESTAMP__", "__traits", "__vector", "__VENDOR__",
+        "__VERSION__", ",", ".", "..", "...", "/", "/=", "!", "!<", "!<=", "!<>",
+        "!<>=", "!=", "!>", "!>=", "$", "%", "%=", "&", "&&", "&=", "(", ")", "*",
+        "*=", "+", "++", "+=", "-", "--", "-=", ":", ";", "<", "<<", "<<=", "<=",
+        "<>", "<>=", "=", "==", "=>", ">", ">=", ">>", ">>=", ">>>", ">>>=", "?",
+        "@", "[", "]", "^", "^=", "^^", "^^=", "{", "|", "|=", "||", "}", "~",
+        "~="];
+    return fixedLengthTokens.map!(a => format(`case tok!"%s": return %d;`, a, a
+        .length)).join("\n\t");
 }
