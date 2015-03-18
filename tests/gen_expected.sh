92ee9f13a8543edba88f84186958135332e42f3a
Hackerpilot <briancschott@gmail.com>
2015-03-18 16:21:49 -0700

    Add output to expected output generator

--- tests/gen_expected.sh
+++ tests/gen_expected.sh
@@ -1,2 +1,11 @@
 dfmt --braces=allman $1.d > allman/$1.d.ref
 dfmt --braces=otbs $1.d > otbs/$1.d.ref
+
+echo "------------------"
+echo "allman:"
+echo "------------------"
+cat allman/$1.d.ref
+echo "------------------"
+echo "otbs:"
+echo "------------------"
+cat otbs/$1.d.ref
