b3946b75253ef680363619b9e75221e36fa6253e
sobaya <sobaya007@gmail.com>
2019-08-03 08:55:32 +0900

    Add: Test for nested struct formatting

--- tests/issue0256.d
+++ tests/issue0256.d
@@ -1,5 +1,8 @@
 void main() {
-    S s = {
+    S s1 = { a: 3 };
+    S s2 = { a: 3, b:"test string" };
+    S s3 = { a: 3, b:"test string", c: {x: 3.14, y: 3 + 4} };
+    T t = {
         someStructMember1: 2,
         someStructMember2: 42,
         someStructMember3: null, // foobar
