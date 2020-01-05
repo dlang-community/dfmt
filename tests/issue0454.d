27929e4cc52e50ac69a3414ca203a6255c23ae21
Eugen Wissner <belka@caraus.de>
2020-01-05 18:52:01 +0100

    Wrap the argument list of a template function
    Fixes #454.

---
+++ tests/issue0454.d
@@ -0,0 +1,9 @@
+void main()
+{
+    format!"%s" //
+        ("");
+    format!("%s") //
+        ("");
+    format!("%s") //
+        ("", argument1, argument2, argument3, argument4, argument5, argument6, argument7, argument8, argument9, argument10);
+}
